e0ae849ab52bd1fc6c20ea93e49ab26f3d144d26
ElliotDaSmol <yoan.le-navenec@epitech.eu>
2026-08-19 06:13:28 +0200

    [UPDATE] following a structural change in the file i'm using for card data, i updated the parsing script to properly handle the new categories. Also updated the banlist data, which was outdated

--- scripts/parseCards.js
+++ scripts/parseCards.js
@@ -3,14 +3,14 @@ const StreamObject = require('stream-json/streamers/StreamObject');
 const fs = require('fs');
 const path = require('path');
 const {pick} = require('stream-json/filters/Pick');
- 
+
 const processedCards = new Map();
- 
+
 const pipeline = fs.createReadStream(path.join(__dirname, '../AllPrintings.json'))
 .pipe(parser())
 .pipe(pick({filter: 'data'}))
 .pipe(StreamObject.make());
- 
+
 // Handle each card object as it is parsed
 pipeline.on('data', data => {
   data.value.cards.forEach(card => {
@@ -31,7 +31,7 @@ pipeline.on('data', data => {
         toughness: card.toughness,
         text: card.text,
         frenchText: frenchData ? frenchData.text : null,
-        flavorNames: card.flavorName ? [card.flavorName] : [],
+        flavorNames: [card.flavorName, card.faceFlavorName, card.printedName, card.facePrintedName].filter(Boolean),
         printings: [{
           artist: card.artist,
           setCode: card.setCode,
@@ -51,10 +51,19 @@ pipeline.on('data', data => {
       if (card.flavorName && !existingCard.flavorNames.includes(card.flavorName)) {
         existingCard.flavorNames.push(card.flavorName)
       }
+      if (card.faceFlavorName && !existingCard.flavorNames.includes(card.faceFlavorName)) {
+        existingCard.flavorNames.push(card.faceFlavorName)
+      }
+      if (card.printedName && !existingCard.flavorNames.includes(card.printedName)) {
+        existingCard.flavorNames.push(card.printedName)
+      }
+      if (card.facePrintedName && !existingCard.flavorNames.includes(card.facePrintedName)) {
+        existingCard.flavorNames.push(card.facePrintedName)
+      }
     }
   });
 });
- 
+
 //write the processed cards to a new JSON when the stream ends
 pipeline.on('end', () => {
     fs.writeFileSync(
